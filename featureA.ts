7aed7a74830eb2a5878d7070820ea955d5af44ec
narendramahajantal <narendra.mahajan@talentica.com>
2017-06-21 22:46:24 +0530

    feature A

---
+++ featureA.ts
@@ -0,0 +1,16 @@
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+
+https://gepmtstorage.blob.core.windows.net/smart2ux/workspace/index_launcher.html#/sourcing/rfx/new
+
+
+XYZ
